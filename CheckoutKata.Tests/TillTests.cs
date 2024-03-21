7422feba6e11916f56a72df65ffa266ef85c9aba
antonio <antonio_mail1994@yahoo.it>
2024-03-21 20:18:29 +0000

    Adding initial tillTest class and adding a reference to the CheckoutKata project, so that we can use the Till class from within the test project.

---
+++ CheckoutKata.Tests/TillTests.cs
@@ -0,0 +1,16 @@
+﻿using CheckoutKata.Interfaces;
+using NUnit.Framework;
+
+namespace CheckoutKata.Tests
+{
+    [TestFixture]
+    public class TillTests
+    {
+        ITill till;
+
+        public TillTests()
+        {
+            till = new Till();
+        }
+    }
+}
